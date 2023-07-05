3fc705da52ddd639b4852d59b7cd7709f414fd5b
lukczar1706 <lcse.2001@gmail.com>
2023-07-05 22:47:14 +0200

    simpe validation and error handling added

--- src/components/ReceiptForm.tsx
+++ src/components/ReceiptForm.tsx
@@ -1,12 +1,21 @@
-import { useRef, useState, useEffect, ChangeEventHandler } from 'react';
+import { useRef, useState, useEffect, FormEventHandler } from 'react';
 import sendReceipt from './sendReceipt';
 import Input from './Input';
 import Select from './Select';
 import './ReceiptForm.scss';
 import Button from './Button';
 
+interface Receipt {
+  id: string;
+  name: string;
+  email: string;
+  treatment: string;
+  price: string;
+}
+
 const ReceiptForm = () => {
-  const [receiptsList, setReceiptsList] = useState(Array<object>); //useState<Receipt[]>([])
+  const [receiptsList, setReceiptsList] = useState<Receipt[]>([]);
+  const [errorMessage, setErrorMessage] = useState<string>('');
 
   const clearInputs = () => {
     if (clientNameRef.current && clientEmailRef.current && treatmentRef.current && priceRef.current) {
@@ -27,14 +36,38 @@ const ReceiptForm = () => {
   const treatmentRef = useRef<HTMLSelectElement>(null);
   const priceRef = useRef<HTMLInputElement>(null);
 
-  const submitHandler = (event: React.FormEvent) => {
-    event.preventDefault();
+  const inputValidator = (): boolean => {
+    if (
+      clientNameRef.current?.value.trim() === '' ||
+      clientEmailRef.current?.value.trim() === '' ||
+      treatmentRef.current?.value === '' ||
+      priceRef.current?.value === ''
+    ) {
+      setErrorMessage('Please fill all fields');
+      return true;
+    }
+    return false;
+  };
 
-    const data = {
-      name: clientNameRef.current?.value,
-      email: clientEmailRef.current?.value,
-      treatment: treatmentRef.current?.value,
-      price: priceRef.current?.value,
+  useEffect(() => {
+    if (errorMessage !== '') {
+      return;
+    }
+  }, [errorMessage]);
+
+  const submitHandler: FormEventHandler<HTMLFormElement> = (event: React.FormEvent) => {
+    event.preventDefault();
+    setErrorMessage('');
+
+    if (inputValidator()) {
+      return;
+    }
+    const data: Receipt = {
+      id: receiptsList.length + 1 + '/' + new Date().getFullYear(),
+      name: clientNameRef.current?.value ?? '',
+      email: clientEmailRef.current?.value ?? '',
+      treatment: treatmentRef.current?.value ?? '',
+      price: priceRef.current?.value ?? '',
     };
 
     // 1.Save array to display and edit if needed, 2.load from the server
@@ -47,19 +80,13 @@ const ReceiptForm = () => {
     clearInputs();
   };
 
-  // const handleChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-  // const handleChange: ChangeEventHandler<HTMLSelectElement> = event => {
-  //   setTreatment(event.target.value);
-  // };
-
   return (
-    //TODO: Add some validation
-
     <form className="receiptForm" onSubmit={submitHandler}>
       <Input ref={clientNameRef} label="Client Name:" type="text" />
       <Input ref={clientEmailRef} label="Client Email:" type="email" />
       <Select ref={treatmentRef} label="Treatment:" options={['lashes', 'brows', 'nails']} />
       <Input ref={priceRef} label="Price:" type="number" />
+      {errorMessage && <div style={{ color: 'red' }}>{errorMessage}</div>}
       <Button className="button is-rounded" type="submit">
         Send
       </Button>
