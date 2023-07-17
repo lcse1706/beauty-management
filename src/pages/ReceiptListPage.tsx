94873b88a8b8da52ef6d7e0d84e136822ab824a7
lukczar1706 <lcse.2001@gmail.com>
2023-07-17 10:20:13 +0200

    check if loggedIn after refresh

--- src/pages/ReceiptListPage.tsx
+++ src/pages/ReceiptListPage.tsx
@@ -1,6 +1,17 @@
+import { useNavigate } from 'react-router';
+import { useAuthContext } from '../components/Auth/AuthContext';
 import ReceiptList from '../components/ReceiptList';
+import { useEffect } from 'react';
 
 const ReceiptListPage = () => {
+  const { isLoggedin } = useAuthContext();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!isLoggedin) {
+      navigate('/');
+    }
+  }, []);
   return <ReceiptList />;
 };
 
--- src/pages/ReceiptPage.tsx
+++ src/pages/ReceiptPage.tsx
@@ -1,7 +1,19 @@
+import { useNavigate } from 'react-router';
+import { useAuthContext } from '../components/Auth/AuthContext';
 import ReceiptForm from '../components/ReceiptForm';
 import './ReceiptPage.scss';
+import { useEffect } from 'react';
 
 const ReceiptPage = () => {
+  const { isLoggedin } = useAuthContext();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!isLoggedin) {
+      navigate('/');
+    }
+  }, []);
+
   return (
     <div className="receiptWrapper">
       <h1>Receipt Page</h1>
