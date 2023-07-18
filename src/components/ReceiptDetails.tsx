1e527dcb5c1a9d02354f9de0af0bbdd20c0e2352
lukczar1706 <lcse.2001@gmail.com>
2023-07-18 22:24:04 +0200

    data object pass

--- src/components/ReceiptDetails.tsx
+++ src/components/ReceiptDetails.tsx
@@ -1,25 +1,40 @@
+import { Button } from '../UI/Button';
+import { useNavigate } from 'react-router';
 import './ReceiptDetails.scss';
 
 type Props = {
   data: {
-    receipt_id: string;
-    name: string;
-    email: string;
-    threatment: string;
-    price: number;
-    date: string;
+    id: string;
+    fields: {
+      receipt_id: string;
+      name: string;
+      email: string;
+      threatment: string;
+      price: number;
+      date: string;
+    };
   };
 };
 
 export const ReceiptDetails = ({ data }: Props) => {
+  const navigate = useNavigate();
+
+  const goToDetails = () => {
+    navigate(`/receiptlist/${data.id}`);
+    console.log(data.id);
+  };
+
   return (
     <div className="details">
-      <p>{data.receipt_id}</p>
-      <p>{data.name}</p>
-      <p>{data.email}</p>
-      <p>{data.threatment}</p>
-      <p>{data.price}</p>
-      <p>{data.date}</p>
+      <p>{data.fields.receipt_id}</p>
+      <p>{data.fields.name}</p>
+      <p>{data.fields.email}</p>
+      <p>{data.fields.threatment}</p>
+      <p>{data.fields.price}</p>
+      <p>{data.fields.date}</p>
+      <Button type="button" onClick={goToDetails}>
+        info
+      </Button>
     </div>
   );
 };
