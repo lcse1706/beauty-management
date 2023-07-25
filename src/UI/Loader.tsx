567e71df6f4fdadc13096461ab0a0cf28e1ba112
lukczar1706 <lcse.2001@gmail.com>
2023-07-25 18:01:15 +0200

    Indicator implemented

---
+++ src/UI/Loader.tsx
@@ -0,0 +1,22 @@
+import './Loader.scss';
+
+export const Loader = () => {
+  return (
+    <div className="loadingio-spinner-spinner-krznzwxcko">
+      <div className="ldio-eq6nssjcju">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  );
+};
