0bac6ff3dcd338ca4920dcbe720df8c66cf41103
lukczar1706 <lcse.2001@gmail.com>
2023-07-25 18:00:36 +0200

    loading state added

--- src/components/Context/DataContext.tsx
+++ src/components/Context/DataContext.tsx
@@ -17,6 +17,8 @@ interface DataContextType {
   setReceipts: React.Dispatch<React.SetStateAction<Receipt[]>>;
   receiptId: string;
   setReceiptId: React.Dispatch<React.SetStateAction<string>>;
+  loading: boolean;
+  setLoading: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
 const DataContext = createContext<DataContextType | null>(null);
@@ -32,8 +34,11 @@ export const useDataContext = () => {
 export const DataProvider = ({ children }: { children: React.ReactNode }) => {
   const [receipts, setReceipts] = useState<Receipt[]>([]);
   const [receiptId, setReceiptId] = useState<string>('');
+  const [loading, setLoading] = useState(true);
 
   return (
-    <DataContext.Provider value={{ receipts, setReceipts, receiptId, setReceiptId }}>{children}</DataContext.Provider>
+    <DataContext.Provider value={{ receipts, setReceipts, receiptId, setReceiptId, loading, setLoading }}>
+      {children}
+    </DataContext.Provider>
   );
 };
