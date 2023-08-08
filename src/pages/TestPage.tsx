c5ad22eb210c1273732cb09cf6947217e3880f17
lukczar1706 <65828191+lcse1706@users.noreply.github.com>
2023-08-08 10:06:11 +0200

    Delete TestPage.tsx

--- src/pages/TestPage.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { useForm } from 'react-hook-form';
-import { Warning } from '../stories/Button.stories';
-import { Button } from '../stories/Button';
-
-export const TestPage = () => {
-  return <Button backgroundColor="red" label="Delete now" onClick={() => {}} primary />;
-};
