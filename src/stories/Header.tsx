35c29d6325b2ca88175bbc5a53b4fddd78f9bdab
lukczar1706 <lcse.2001@gmail.com>
2023-08-09 21:49:14 +0200

    cosmetics

--- src/stories/Header.tsx
+++ src/stories/Header.tsx
@@ -1,9 +1,11 @@
 import type { ComponentProps } from 'react';
+import logo from './../components/layout/logowhite.png';
 import './header.css';
 
 export const Header = ({ children }: ComponentProps<'header'>) => {
   return (
     <header className="mainNavHeader">
+      {/* <img className="scale" src={logo} alt="Logo Beauty by EC" /> */}
       <h1>Beauty Management</h1>
       {children}
     </header>
