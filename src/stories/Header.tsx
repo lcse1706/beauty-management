9615636133f289fd0cd57376400c35fd0e52b28b
lukczar1706 <lcse.2001@gmail.com>
2023-08-10 01:06:58 +0200

    logo added

--- src/stories/Header.tsx
+++ src/stories/Header.tsx
@@ -1,12 +1,14 @@
 import type { ComponentProps } from 'react';
-import logo from './../components/layout/logowhite.png';
+import logo from './../components/layout/diamentnobgwhite.png';
 import './header.css';
 
 export const Header = ({ children }: ComponentProps<'header'>) => {
   return (
     <header className="mainNavHeader">
-      {/* <img className="scale" src={logo} alt="Logo Beauty by EC" /> */}
-      <h1>Beauty Management</h1>
+      <h1>
+        <img className="scale" src={logo} alt="Logo Beauty by EC" />
+        Beauty Management
+      </h1>
       {children}
     </header>
   );
