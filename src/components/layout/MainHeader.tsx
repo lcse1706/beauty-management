e7b923a388f4dc03b346c674655ceb6b636a5655
lukczar1706 <lcse.2001@gmail.com>
2023-08-10 14:12:34 +0200

    cleanup

--- src/components/layout/MainHeader.tsx
+++ src/components/layout/MainHeader.tsx
@@ -2,7 +2,7 @@ import { Navigation } from './Navigation';
 import './MainHeader.scss';
 import { Header } from '../../stories/Header';
 
-//TODO Do usuniecia, jest w storkach
+//TOASK Do usuniecia, jest w storkach
 
 const MainHeader = () => {
   return (
