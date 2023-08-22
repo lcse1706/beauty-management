832a23dfb5c57c2bbd97d247529c037a3b928385
lukczar1706 <lcse.2001@gmail.com>
2023-08-22 19:09:34 +0200

    env problem fixed

--- src/services/receipts.ts
+++ src/services/receipts.ts
@@ -36,11 +36,9 @@ interface ReceiptToAirTable {
   fields: Receipt;
 }
 
-const BASE_URL = 'https://api.airtable.com/v0/appzpLACufTjr6Q8g/receipts';
-
 export const fetchReceipts = async () => {
-  // const response: Response = await fetch(`${process.env.AIRTABLE_BASE_URL}`, {
-  const response: Response = await fetch(BASE_URL, {
+  const response: Response = await fetch(`${process.env.REACT_APP_AIRTABLE_BASE_URL}`, {
+    // const response: Response = await fetch(BASE_URL, {
     headers: {
       Authorization: `Bearer ${process.env.REACT_APP_AIRTABLE_API_TOKEN}`,
     },
@@ -70,7 +68,7 @@ export const sendReceipt = async (receipt: Receipt) => {
     'Content-Type': 'application/json',
   };
 
-  const sendResponse: Response = await fetch(BASE_URL, {
+  const sendResponse: Response = await fetch(`${process.env.REACT_APP_AIRTABLE_BASE_URL}`, {
     method: 'POST',
     headers: headers,
     body: JSON.stringify(ReceiptAirTableFormat.parse(receiptToAirTable)),
@@ -97,7 +95,7 @@ export const updateRecord = async (recordId: string, receipt: Receipt) => {
   };
 
   try {
-    const url = `${BASE_URL}/${recordId}`;
+    const url = `${process.env.REACT_APP_AIRTABLE_BASE_URL}/${recordId}`;
     const headers = {
       Authorization: `Bearer ${process.env.REACT_APP_AIRTABLE_API_TOKEN}`,
       'Content-Type': 'application/json',
@@ -120,7 +118,7 @@ export const updateRecord = async (recordId: string, receipt: Receipt) => {
 
 export const deleteReceipt = async (recordId: string) => {
   try {
-    const url = `${BASE_URL}/${recordId}`;
+    const url = `${process.env.REACT_APP_AIRTABLE_BASE_URL}/${recordId}`;
     const headers = {
       Authorization: `Bearer ${process.env.REACT_APP_AIRTABLE_API_TOKEN}`,
     };
