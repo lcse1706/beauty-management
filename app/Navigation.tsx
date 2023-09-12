26a8d4f271c207ba55ceac86f054cee6c5e54f80
lukczar1706 <lcse.2001@gmail.com>
2023-09-12 20:47:27 +0200

    active-class implemented

--- app/Navigation.tsx
+++ app/Navigation.tsx
@@ -1,16 +1,18 @@
 'use client';
 
 import Link from 'next/link';
-import { useRouter } from 'next/navigation';
-
-// import { useNavigate, NavLink } from 'react-router-dom';
+import { useRouter, usePathname } from 'next/navigation';
 import { Button } from '../components/ui';
 import { useAuthContext } from '../context/AuthContext';
-import './Navigation.scss';
 
 export const Navigation: React.FC = () => {
   const { isLogged, setIsLogged } = useAuthContext();
   const router = useRouter();
+  const path = usePathname();
+
+  const isActive = (href: string) => {
+    return path === href ? 'active-class' : '';
+  };
 
   const logoutHandler = () => {
     setIsLogged(false);
@@ -18,19 +20,25 @@ export const Navigation: React.FC = () => {
     router.push('/');
   };
 
-  const classes = 'text-white hover:text-gray-400 font-medium';
-
   return (
     <nav className="flex flex-row">
       {isLogged && (
         <ul className="flex flex-row items-center space-x-5  ">
           <li className="">
-            {/* <NavLink to="/sendreceipt" className={({ isActive }) => `${classes} ${isActive ? 'active-class' : ''}`}> */}
-            <Link href="/sendreceipt">Send Receipt</Link>
+            <Link
+              href="/sendreceipt"
+              className={`text-white hover:text-gray-400 font-medium ${isActive('/sendreceipt')}`}
+            >
+              Send Receipt
+            </Link>
           </li>
           <li>
-            {/* <NavLink to="/receiptlist" className={({ isActive }) => `${classes} ${isActive ? 'active-class' : ''}`}> */}
-            <Link href="/receiptlist">Receipts</Link>
+            <Link
+              href="/receiptlist"
+              className={`text-white hover:text-gray-400 font-medium ${isActive('/receiptlist')}`}
+            >
+              Receipts
+            </Link>
           </li>
           <li>
             <Button label="Log Out" onClick={logoutHandler} />
