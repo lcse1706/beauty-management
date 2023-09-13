83b40510d6ee470636209df484fadc9ea7cb8c72
lukczar1706 <lcse.2001@gmail.com>
2023-09-13 21:27:37 +0200

    memoizing

--- components/ui/Button/Button.tsx
+++ components/ui/Button/Button.tsx
@@ -10,10 +10,10 @@ interface ButtonProps {
 
 const BASE_BUTTON_CLASSES = 'bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full m-1';
 
-export const Button = ({ label, className, ...rest }: ComponentProps<'button'> & ButtonProps) => {
+export const Button = React.memo(({ label, className, ...rest }: ComponentProps<'button'> & ButtonProps) => {
   return (
     <button type="button" className={[BASE_BUTTON_CLASSES, className].filter(Boolean).join(' ')} {...rest}>
       {label}
     </button>
   );
-};
+});
