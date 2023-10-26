931efba071fff01d93c02323e038291643439916
lukczar1706 <lcse.2001@gmail.com>
2023-10-26 15:55:27 +0200

    mediaquery for mobile

--- app/receiptlist/ReceiptList.tsx
+++ app/receiptlist/ReceiptList.tsx
@@ -8,14 +8,17 @@ import { useRouter } from 'next/navigation';
 import { Loader } from '@/components/ui';
 import { sortReceipts } from '@/components/utils/sortReceipts';
 import { useAuthContext, useDataContext, usePopupContext } from '@/context/';
+import { useMediaQuery } from '@/hooks/useMediaQuery';
 import { fetchReceipts } from '@/services/receipts';
 
 import { ReceiptDisplayForm } from './ReceiptDisplayForm';
 import './ReceiptList.css';
+import { ReceiptDisplayFormMobile } from './RecieiptDisplayFormMobile';
 
 export const ReceiptList = () => {
     const { receipts, setReceipts, loading, setLoading } = useDataContext();
     const { setShowPopup, setMessage } = usePopupContext();
+    const breakpoint768 = useMediaQuery(768);
 
     //Moved from page.tsx to hide useEffect in client component
 
@@ -55,9 +58,16 @@ export const ReceiptList = () => {
             {loading ? (
                 <Loader />
             ) : (
-                sortedReceipts.map((receipt) => (
-                    <ReceiptDisplayForm key={receipt.id} data={receipt} />
-                ))
+                sortedReceipts.map((receipt) =>
+                    breakpoint768 ? (
+                        <ReceiptDisplayFormMobile
+                            key={receipt.id}
+                            data={receipt}
+                        />
+                    ) : (
+                        <ReceiptDisplayForm key={receipt.id} data={receipt} />
+                    ),
+                )
             )}
         </ul>
     );
---
+++ app/receiptlist/RecieiptDisplayFormMobile.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+import { useRouter } from 'next/navigation';
+
+import { Button } from '@/components/ui';
+import { useDataContext } from '@/context/';
+import { ReceiptDisplayF } from '@/lib/types';
+
+export const ReceiptDisplayFormMobile = ({ data }: ReceiptDisplayF) => {
+    const router = useRouter();
+    const { setReceiptId } = useDataContext();
+
+    const goToDetails = () => {
+        setReceiptId(data.id);
+        router.push(`/receiptlist/${data.id}`);
+    };
+
+    return (
+        <li className="flex flex-col items-center justify-center border-b-2 w-11/12 bg-white rounded-lg shadow-md border border-gray-200">
+            <p className=" text-gray-600">{data.fields.receipt_id}</p>
+            <p className=" text-gray-800 font-medium">{data.fields.name}</p>
+            {/* <p className="w-1/4 text-gray-600">{data.fields.email}</p> */}
+            {/* <p className="w-1/4 text-gray-600">{data.fields.treatment}</p> */}
+            {/* <p className="w-1/4 text-gray-800 font-bold"> */}
+            {/* {data.fields.price} kr */}
+            {/* </p> */}
+            {/* <p className="w-1/4 text-gray-600">{data.fields.date}</p> */}
+            <Button
+                label="details"
+                onClick={goToDetails}
+                className="mt-2 bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded"
+            />
+        </li>
+    );
+};
