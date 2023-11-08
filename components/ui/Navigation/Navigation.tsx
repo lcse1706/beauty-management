6c1719b232c88348fe43c7cd07a1a4f507d8a1cd
lukczar1706 <lcse.2001@gmail.com>
2023-11-08 19:44:00 +0100

    cleanup

--- components/ui/Navigation/Navigation.tsx
+++ components/ui/Navigation/Navigation.tsx
@@ -1,9 +1,5 @@
 'use client';
 
-import Link from 'next/link';
-import { usePathname, useRouter } from 'next/navigation';
-
-import { Button } from '@/components/ui';
 import { useAuthContext } from '@/context';
 import { useMediaQuery } from '@/hooks/useMediaQuery';
 
@@ -11,20 +7,9 @@ import { NavigationDesktop } from './NavigationDesktop';
 import { NavigationMobile } from './NavigationMobile';
 
 export const Navigation: React.FC = () => {
-    const { isLogged, setIsLogged } = useAuthContext();
-    // const router = useRouter();
-    // const path = usePathname();
+    const { isLogged } = useAuthContext();
+
     const breakpoint768 = useMediaQuery(768);
-
-    // const isActive = (href: string) => {
-    //     return path === href ? 'border-b border-white' : '';
-    // };
-
-    // const logoutHandler = () => {
-    //     setIsLogged(false);
-    //     sessionStorage.setItem('isAuth', 'false');
-    //     router.push('/');
-    // };
 
     return (
         <nav className="flex flex-row">
