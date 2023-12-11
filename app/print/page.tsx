64ffb6f037006ada6058139687690280434f55e5
lukczar1706 <lcse.2001@gmail.com>
2023-12-11 21:10:01 +0100

    print page updated

--- app/print/page.tsx
+++ app/print/page.tsx
@@ -1,7 +1,9 @@
 'use client';
 
 import Image from 'next/image';
+import { useRouter } from 'next/navigation';
 
+import { Button } from '@/components/ui';
 import { Receipt } from '@/lib';
 
 import logo from './diamentnobg.png';
@@ -9,6 +11,7 @@ import logo from './diamentnobg.png';
 // This component is adjusted to handy printer which can print receipt from App with build-in browser.
 
 const PrintPage = () => {
+    const router = useRouter();
     let storedData: string | undefined;
 
     if (typeof window !== 'undefined') {
@@ -16,6 +19,13 @@ const PrintPage = () => {
     }
 
     const data: Receipt = JSON.parse(storedData ?? '{}');
+
+    const backHandler = () => {
+        router.back();
+    };
+    const printHandler = () => {
+        window.print();
+    };
 
     const currentDate =
         new Date().getDate() +
@@ -25,45 +35,49 @@ const PrintPage = () => {
         new Date().getFullYear();
 
     return (
-        <html className="bg-white text-black text-2xl" lang="en">
-            <body>
-                <div className="container print:w-[4cm] print:h-[10cm] print:p-4">
-                    <div>
-                        <p className="mb-5">
-                            <span className="font-bold">Datum: </span>{' '}
-                            {currentDate}
-                        </p>
-                        <p className="text-lg font-bold">
-                            <Image
-                                src={logo}
-                                alt="Logo Beauty by EC"
-                                blurDataURL="blur"
-                                width={70}
-                                className="inline mr-3 text-black"
-                            />{' '}
-                            Beauty By Ec
-                        </p>
-                        <p>Centrumvägen 17, 619 71 Vagnhärad</p>
-                        <p className="mb-1">SE901115750101</p>
-                    </div>
+        <div>
+            <div className="max-w-md mx-auto bg-white p-8 border border-gray-600 text-black mb-3">
+                <p className="text-lg font-bold mb-3">
+                    <Image
+                        src={logo}
+                        alt="Logo Beauty by EC"
+                        blurDataURL="blur"
+                        width={70}
+                        className="inline mr-3 text-black"
+                    />
+                    Beauty By Ec
+                </p>
+                <p>Gnestavägen 2c, 619 71 Vagnhärad</p>
+                <p>SE901115750101</p>
+                <hr className="my-5 border-t border-gray-600" />
 
-                    <div>
-                        <p className="mb-1">
-                            <span className="font-bold">Kvittens Nummer: </span>
-                            {data.receipt_id}
-                        </p>
-                        <p className="mb-1 font-bold">Namn:</p>
-                        <p className="mb-2">{data.name}</p>
-                        <p className="mb-1 font-bold">Behandling:</p>
-                        <p className="mb-2">{data.treatment}</p>
-                        <p className="mb-1">
-                            <span className="font-bold">Pris: </span>
-                            {data.price} kr
-                        </p>
-                    </div>
-                </div>
-            </body>
-        </html>
+                <p className="mb-1">
+                    <span className="font-bold">Date: </span> {currentDate}
+                </p>
+                <p className="mb-1">
+                    <span className="font-bold">Receipt Number: </span>
+                    {data.receipt_id}
+                </p>
+                <p className="mb-1">
+                    <span className="font-bold">Name: </span>
+                    {data.name}
+                </p>
+                <p className="mb-1">
+                    <span className="font-bold">Treatment: </span>
+                    {data.treatment}
+                </p>
+                <p className="mb-3">
+                    <span className="font-bold">Price: </span>
+                    {data.price} SEK
+                </p>
+            </div>
+            <Button label="<<<" onClick={backHandler} />
+            <Button
+                label="Print"
+                className="bg-green-600 hover:bg-green-700"
+                onClick={printHandler}
+            />
+        </div>
     );
 };
 
