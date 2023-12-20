bc282860615caf61c196db9bce2472871f2f4448
lukczar1706 <lcse.2001@gmail.com>
2023-12-20 21:53:11 +0100

    cleanup

--- app/receiptlist/page.tsx
+++ app/receiptlist/page.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
+
 import { ReceiptList } from './ReceiptList';
 
 const ReceiptListPage = () => {
-  return (
-    <div>
-      <h2 className="text-xl color-black font-bold mb-4 text-center text-white">Receipts</h2>
-      <ReceiptList />
-    </div>
-  );
+    return (
+        <div>
+            <h2 className="text-xl color-black font-bold mb-4 text-center text-white">
+                Receipts
+            </h2>
+            <ReceiptList />
+        </div>
+    );
 };
 
 export default ReceiptListPage;
