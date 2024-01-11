30917279fbfaf42c9e1999db3fb3950669f08139
lukczar1706 <lcse.2001@gmail.com>
2024-01-11 21:13:44 +0100

    wrap story in PopupProvider

--- components/ui/Popup/Popup.stories.tsx
+++ components/ui/Popup/Popup.stories.tsx
@@ -1,30 +1,31 @@
-import type { Meta, StoryObj } from '@storybook/react';
-import { MockPopupProvider } from './mockPopupProvider';
-import { usePopupContext } from './mockPopupProvider';
-import { Popup } from './Popup';
 import React from 'react';
 
-const meta = {
-  title: 'UI/Popup',
-  component: Popup,
+import type { Meta, StoryObj } from '@storybook/react';
 
-  decorators: [
-    (Story: any) => (
-      <MockPopupProvider>
-        <Story />
-      </MockPopupProvider>
-    ),
-  ],
-  parameters: {
-    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/react/configure/story-layout
-    layout: '',
-  },
-  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
-  tags: ['autodocs'],
-  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
-  argTypes: {
-    // backgroundColor: { control: 'color' },
-  },
+import { PopupProvider, usePopupContext } from '../../../context';
+import { Popup } from './Popup';
+
+const meta = {
+    title: 'UI/Popup',
+    component: Popup,
+
+    // decorators: [
+    //   (Story: any) => (
+    //     <MockPopupProvider>
+    //       <Story />
+    //     </MockPopupProvider>
+    //   ),
+    // ],
+    parameters: {
+        // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/react/configure/story-layout
+        layout: '',
+    },
+    // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
+    tags: ['autodocs'],
+    // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+    argTypes: {
+        // backgroundColor: { control: 'color' },
+    },
 } satisfies Meta<typeof Popup>;
 
 export default meta;
@@ -34,7 +35,19 @@ type Story = StoryObj<typeof meta>;
 //   args: {},
 // };
 
-export const Default: Story = (args: any) => <Popup {...args} />;
+const storybookValues: any = {
+    popup: true,
+    showPopup: () => {}, // You can provide a function or leave it empty
+    hidePopup: () => {}, // You can provide a function or leave it empty
+    message: 'Hello From StoryBook ! ',
+    setMessage: () => {}, // You can provide a function or leave it empty
+};
+
+export const Default: Story = (args: any) => (
+    <PopupProvider storybookValues={storybookValues}>
+        <Popup {...args} />
+    </PopupProvider>
+);
 Default.args = {
-  useContext: usePopupContext,
+    useContext: usePopupContext,
 };
